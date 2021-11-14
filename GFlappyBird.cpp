fa26a236f14d01304b0307e46f1fa1b2894927cb
nik <sparrowflyy@gmail.com>
2021-11-15 01:08:18 +0700

    Basic class initialized

--- GFlappyBird.cpp
+++ GFlappyBird.cpp
@@ -1,15 +1,23 @@
 //
 // Created by nik on 14.11.2021.
 //
-
+const int eventsCount = 2;
+const int objectsCount = 2;
 #include "GFlappyBird.h"
+#include "FBird.h"
 GFlappyBird::GFlappyBird(int iWinWidth, int iWinHeight) {
   winWidth = iWinWidth;
   winHeight = iWinHeight;
+  eventsPool.resize(eventsCount);
+  objects.resize(objectsCount);
+  init();
+  eventsPool[Gravity] = std::make_shared<GEventForce>(FB::gravity);
+  eventsPool[Momentum] = std::make_shared<GEventForce>(FB::momentum);
 }
 
 void GFlappyBird::init() {
-
+  auto bird = std::make_shared<FBird>();
+  objects[Bird] = bird;
 }
 
 void GFlappyBird::drawObjects(sf::RenderTarget &target, sf::RenderStates states) {
--- GFlappyBird.h
+++ GFlappyBird.h
@@ -4,6 +4,10 @@
 #include "GEngine/GGame.h"
 #ifndef FLAPPY_BIRD_GFBIRD_H
 #define FLAPPY_BIRD_GFBIRD_H
+namespace FB{
+    const sf::Vector2f gravity(0.0,1.0);
+    const sf::Vector2f momentum(0.0,-1.0);
+};
 class GFlappyBird: public GGame{
 public:
     explicit GFlappyBird(int iWinWidth = 600, int iWinHeight = 500);
@@ -12,6 +16,15 @@ public:
     void processKeys(const sf::Event& event) override;
     void processEvents() override;
     void postProcess() override;
-    ~GFlappyBird() ;
+    ~GFlappyBird();
+private:
+    enum Events{
+        Gravity,
+        Momentum
+    };
+    enum Objects{
+        Background,
+        Bird
+    };
 };
 #endif //FLAPPY_BIRD_GFBIRD_H
